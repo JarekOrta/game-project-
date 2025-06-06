1fae07e65266164ef8a7dbd858c272b40c6e4606
Max-Wu000 <mwu76@calpoly.edu>
2025-06-06 09:26:10 -0700

    Update test_zombie.py

--- final project/test_zombie.py
+++ final project/test_zombie.py
@@ -11,26 +11,26 @@ class TestZombie(unittest.TestCase):
         self.zombie = Zombie("Walker", 50, 8)
 
     def test_initial_attributes(self):
-        """test that a new Zombie has correct default stats."""
+        #test that a new Zombie has correct default stats.
         self.assertEqual(self.zombie.name, "Walker")
         self.assertEqual(self.zombie.hp, 50)
         self.assertEqual(self.zombie.attack_power, 8)
 
     def test_take_damage(self):
-        """test that take_damage reduces HP appropriately and floors at 0."""
+        #test that take_damage reduces HP appropriately and floors at 0.
         self.zombie.take_damage(10)
         self.assertEqual(self.zombie.hp, 40)
         self.zombie.take_damage(50)
         self.assertEqual(self.zombie.hp, 0)
 
     def test_is_alive(self):
-        """test is_alive returns correct boolean"""
+        #test is_alive returns correct boolean
         self.assertTrue(self.zombie.is_alive())
         self.zombie.hp = 0
         self.assertFalse(self.zombie.is_alive())
 
     def test_attack_damage(self):
-        """test attack() returns correct damage value"""
+        #test attack() returns correct damage value
         damage = self.zombie.attack(None)
         self.assertEqual(damage, 8)
 
